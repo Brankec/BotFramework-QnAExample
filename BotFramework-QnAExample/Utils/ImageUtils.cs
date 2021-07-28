d9c9840c5e7358d2982887aece79fecac0852203
Brandon <Brandon@DESKTOP-SIHAS9R>
2021-07-28 21:39:43 +0200

    added ImageUtils class

---
+++ BotFramework-QnAExample/Utils/ImageUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace BotFramework_QnAExample.Util.Image
+{
+    public static class ImageUtils
+    {
+        public static Dictionary<string, string> contentTypes = new()
+        {
+            {"image/jpeg", ".jpeg"},
+            {"image/gif", ".gif"},
+            {"image/png", ".png"}
+        };
+        
+        public static byte[] SaveImageFromUrl(string imageUrl, string contentType)
+        {
+            byte[] imageBytes;
+            using (WebClient client = new WebClient())
+            {
+                var type = GetImageTypeFromContentType(contentType);
+                imageBytes = client.DownloadData(new Uri(imageUrl + type));
+            }
+
+            return imageBytes;
+        }
+
+        public static string GetImageTypeFromContentType(string contentType)
+        {
+            var type = contentTypes.FirstOrDefault(x => x.Equals(contentType)).Value;
+
+            return type;
+        }
+
+        public static bool IsContentTypeImage(string contentType)
+        {
+            var isImage = contentTypes.ContainsKey(contentType);
+
+            return isImage;
+        }
+    }
+}
